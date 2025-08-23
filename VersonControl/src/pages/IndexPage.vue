575ec7aeb1bd11d2f5ed85a4cd1ff7594e770d70
RattyC <rattapoommaru@gmail.com>
2025-08-23 21:48:27 +0700

    Change label to Thai language in IndexPage.vue

--- VersonControl/src/pages/IndexPage.vue
+++ VersonControl/src/pages/IndexPage.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="q-pa-md" style="max-width: 400px">
-    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-      <q-input filled v-model="name" label="Your name * ******" hint="Name" lazy-rules
-        :rules="[val => val && val.length > 0 || 'Please type something']" />
-      <q-input filled type="number" v-model="age" label=" age *****" lazy-rules :rules="[
-        val => val !== null && val !== '' || 'Please type your age',
-        val => val > 0 && val < 100 || 'Please type a real age'
-      ]" />
-      <q-toggle v-model="accept" label="I accept the license and terms" />
+
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="name"
+        label="ชื่อ-สกุล *"
+        hint="ชื่อและนามสกุล"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'กรุณาพิมพ์ชื่อ']"
+      />
+
+      <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="อายุ *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'กรุณาใส่อายุ',
+          val => val > 0 && val < 100 || 'กรุณาใส่อายุจริง'
+        ]"
+      />
+
+      <q-toggle v-model="accept" label="ยอมรับ" />
+
       <div>
-        <q-btn label="Submit" type="submit" color="primary" />
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="ยอมรับ" type="submit" color="primary"/>
+        <q-btn label="ยกเลิก" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
+
   </div>
 </template>
 
@@ -21,7 +43,7 @@ import { useQuasar } from 'quasar'
 import { ref } from 'vue'
 
 export default {
-  setup() {
+  setup () {
     const $q = useQuasar()
 
     const name = ref(null)
@@ -31,13 +53,13 @@ export default {
       name,
       age,
       accept,
-      onSubmit() {
+      onSubmit () {
         if (accept.value !== true) {
           $q.notify({
             color: 'red-5',
             textColor: 'white',
             icon: 'warning',
-            message: 'You need to accept the license and terms first'
+            message: 'คุณจำเป็นต้องยอมรับ'
           })
         }
         else {
@@ -45,11 +67,11 @@ export default {
             color: 'green-4',
             textColor: 'white',
             icon: 'cloud_done',
-            message: 'Submitted'
+            message: 'ข้อมูลได้รับการยืนยัน'
           })
         }
       },
-      onReset() {
+      onReset () {
         name.value = null
         age.value = null
         accept.value = false
